ae79f3a4fbf4b730aa3028e23e6e50bd8254d477
securekim <admin@securekim.com>
2018-05-24 21:31:42 +0900

    [Feature] Change certificate structure with CA

--- src/utils.js
+++ src/utils.js
@@ -45,6 +45,44 @@ function generatePin(data){
     
 }
 
+function CERT_createKey(CN,keylen,callback){
+    exec('openssl genrsa -out certs/'+CN+'.key '+keylen, function(error, stdout, stderr) {
+        if(error !== null) {
+            console.log("Create Cert Key : " + error);
+            callback({fail:true,error:error});
+        } else {
+            callback({fail:false,error:"none"});
+        }
+    });
+}
+
+function CERT_csr(CN,callback){
+    exec('openssl req -new -key certs/'+CN+'.key -nodes -subj "/C=KR/O=noweek, Inc./OU=www.securekim.com/OU=(c) 2018 noweek, Inc./CN='+CN+'" -out certs/'+CN+'.csr', function(error, stdout, stderr) {
+        if(error !== null) {
+            console.log("Create CSR : " + error);
+            callback({fail:true,error:error});
+        } else {
+            callback({fail:false,error:"none"});
+        }
+    });         
+}
+
+function CERT_sign(CSR,CA,days,callback){
+    //openssl x509 -req -days 500 -in demo.csr -CA CA-CA.crt -CAkey root.key -CAcreateserial -out my.crt
+    var execstr = 'openssl x509 -req -days '+days+' -in certs/'+CSR+'.csr -signkey certs/'+CA+'.key -out certs/'+CSR+'.pem';
+    if(CSR !== CA) execstr = 'openssl x509 -req -days '+days+' -in certs/'+CSR+'.csr -CA certs/'+CA+'.pem -CAkey certs/'+CA+'.key -CAcreateserial -out certs/'+CSR+'.pem'; 
+    exec(execstr, function(error, stdout, stderr) {
+        if(error !== null) {
+            console.log("Sign CSR : " + error);
+            callback({fail:true,error:error});
+        } else {
+            callback({fail:false,error:"none"});
+        }
+    });  
+}
+
+
+/*
 function createCert(CN,callback){
     result = {fail:1,result:"none"};
     exec('openssl genrsa -out certs/'+CN+'.key 4096', function(error, stdout, stderr) {
@@ -94,6 +132,54 @@ function createCert(CN,callback){
             });  
         }
     });
+}
+*/
+///for command line
+
+function CERT_createCA(CN,callback){
+    CERT_createKey(CN,4096,(result)=>{
+        if(result.fail) return callback(false);
+        CERT_csr(CN,(result)=>{
+            if(result.fail) return callback(false);
+            CERT_sign(CN,CN,36500,(result)=>{
+                if(result.fail) return callback(false);
+                else return callback(true);
+            })
+        });
+    });
+}
+
+function CERT_createCERT(CA,CN,callback){
+    CERT_createKey(CN,2048,(result)=>{
+      if(result.fail) return callback(false);
+      CERT_csr(CN,(result)=>{
+        if(result.fail) return callback(false);
+        CERT_sign(CN,CA,14,(result)=>{
+            if(result.fail) return callback(false);
+            else return callback(true);
+        });
+      });
+    });
+}
+
+function callbacktest(callback){
+    return callback(1); //if no return, below code will be executed.
+    console.log("dont"); 
+}
+
+if(process.argv.length >2){
+    //node utils.js washer --> args[2]
+    //make washerCA key, csr, selfsigned cert
+    //make washer key, csr, signed by washerCA
+    console.log(process.argv[2]);
+    CERT_createCA(process.argv[2]+"-CA",(result)=>{
+        if(result){
+            CERT_createCERT(process.argv[2]+"-CA",process.argv[2],(result)=>{
+                console.log("MAKE CERTIFICATE SIGNED BY CA :"+result);
+            });
+        }
+    });
+
 }
 
 
